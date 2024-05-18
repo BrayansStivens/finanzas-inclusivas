1390cfdb0a06a677a417edbfbf4c11dea6493756
BrayansStivens <brayanstivens66@gmail.com>
2024-05-18 14:55:45 -0500

    se crea componente nav

--- components/ui/nav/index.tsx
+++ components/ui/nav/index.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import Image from "next/image";
+
 function Nav() {
-  return <div>Nasadaav</div>;
+  return (
+    <>
+      <header className="bg-[#2a63b1] h-[61px] w-full"></header>
+      <nav>
+        <div className="py-7 px-5">
+          <Image
+            src="/assets/logo/fina_mujer_logo.png"
+            alt="logo fina mujer"
+            width={250}
+            height={75}
+          />
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Nav;
