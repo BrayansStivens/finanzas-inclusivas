49ccf8fd3938c20b711f1cec6a65cc403f8620b0
BrayansStivens <brayanstivens66@gmail.com>
2024-05-19 00:57:57 -0500

    home features

---
+++ app/home/components/HomeTextSection.tsx
@@ -0,0 +1,18 @@
+function HomeTextSection() {
+  return (
+    <section id="text" className="p-3 bg-[#d9d9d9]">
+      <p className="text-pretty font-medium lg:text-lg leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+        nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+        volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+        ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+        molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero
+        eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
+        zzril delenit augue duis dolore te feugait nulla facilisi.
+      </p>
+    </section>
+  );
+}
+
+export default HomeTextSection;
---
+++ lib/cardsData.ts
@@ -0,0 +1,46 @@
+export async function getCardsData() {
+  return [
+    {
+      src: "/assets/icons/recomendaciones.svg",
+      alt: "Recomendaciones",
+      text: "Recomendaciones para el uso de la herramienta",
+      width: 51.57,
+      height: 63.09,
+    },
+    {
+      src: "/assets/icons/enfoque.svg",
+      alt: "Interseccionalidad",
+      text: "Enfoque de interseccionalidad",
+      width: 75.98,
+      height: 65.82,
+    },
+    {
+      src: "/assets/icons/resultados.svg",
+      alt: "Resultados",
+      text: "Resultados de la investigación",
+      width: 58.37,
+      height: 58.35,
+    },
+    {
+      src: "/assets/icons/perfilamiento.svg",
+      alt: "Perfilamiento",
+      text: "Perfilamiento de mujeres",
+      width: 51.11,
+      height: 64.16,
+    },
+    {
+      src: "/assets/icons/guia.svg",
+      alt: "Guía",
+      text: "Guía de formación",
+      width: 66.35,
+      height: 66.29,
+    },
+    {
+      src: "/assets/icons/evaluacion.svg",
+      alt: "Evaluación",
+      text: "Instrumento de evaluación",
+      width: 70.53,
+      height: 42.29,
+    },
+  ];
+}
